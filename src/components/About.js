13d8b283306939af6622d82bfa03c58c689cd642
Luke Hayes <lukehayesa@gmail.com>
2023-02-07 12:25:15 -0500

    m

--- src/components/About.js
+++ src/components/About.js
@@ -26,7 +26,7 @@ const About = () => {
          </div>
       
          <div className='about-img'>
-          <img src={AboutImg2} alt="bio image" className='w-[80%] md:ml-auto sm:justify-center'/>
+          <img src={AboutImg2} alt="bio image" className='w-[80%] md:ml-auto sm:content-center'/>
          </div>
          </div>
         </section>
